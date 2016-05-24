8ab155f94775b130d0c5bc4bc4cef5162fd9f1a4
Chris Umbel <chris@chrisumbel.com>
2016-05-24 14:24:29 +0000

    command line args added

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace HelloWorld
 {
     public class Program
     {
         public static void Main(string[] args)
-        {            
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+            	.Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>()
+                .UseConfiguration(config)
                 .Build();
 
             host.Run();            
